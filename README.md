c7f162bcf5f3090b714b8043958b357192dd2b29
Asmahan97 <asmahanhmohamed@gmail.com>
2021-12-05 16:24:55 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-kheap7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-kheap7)
---
+++ script.js
@@ -0,0 +1,7 @@
+// ***Write a function which accepts two numbers as arguments. The function should return the sum of the two numbers***
+
+function sumofNum(num1, num2) {
+  return num1 += num2
+}
+
+console.log(sumofNum(9, 6));
