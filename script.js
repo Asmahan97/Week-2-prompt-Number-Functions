4414e5792408eb4fd691641c73a1acfe626dcc7d
Asmahan97 <asmahanhmohamed@gmail.com>
2021-12-05 16:25:45 +0000

    1 file modified

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function sumofNum(num1, num2) {
 }
 
 console.log(sumofNum(9, 6));
+
+
+// ***Write a function that accepts four arguments, each will be a number. The function should add the first two numbers together; add the third and fourth numbers; and return the result of multiplying both sums together***
+
+function multiplySums(num1, num2, num3, num4) {
+  let sum1 = num1+num2;
+  let sum2 = num3+num4;
+  let finalSum = sum1*sum2;
+
+  return finalSum
+  }
+
+  console.log(multiplySums(1, 2, 10, 10));
